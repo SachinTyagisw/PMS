3059dfde82d34003593a27340a765f3947ba5ad0
sachin tyagi <sachin.tyagisw@gmail.com>
2017-04-26 22:18:36 +0530

    Implementing guestcheckinmanager.js changes

---
+++ PMS.Web/Scripts/GuestCheckinManager.js
@@ -0,0 +1,23 @@
+﻿(function (win) {
+
+    var guestCheckinManager = {
+
+        Initialize: function () {
+            var pmsService = new window.PmsService();
+            var args = {};
+            args.propertyId = 1;
+
+            // getting room data
+            var data = pmsService.GetRoomByProperty(args);
+
+            pmsService.Handlers.OnGetRoomByPropertySuccess = function (data) {
+
+            };
+            pmsService.Handlers.OnGetRoomByPropertyFailure = function () {
+
+            };
+        }
+    };
+
+    win.GuestCheckinManager = guestCheckinManager;
+}(window));
