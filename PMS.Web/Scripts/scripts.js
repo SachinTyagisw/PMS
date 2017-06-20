9c1af957863f5e50809af4c3f5d4686a59c6f0e0
Deepak <deepak.kiet@gmail.com>
2017-06-21 00:38:24 +0530

    Sidebar fixed

--- PMS.Web/Scripts/scripts.js
+++ PMS.Web/Scripts/scripts.js
@@ -6,28 +6,28 @@
     Sidebar open close animated humberger icon
     ------------------------------------------------*/
 
-    $(".hamburger").on('click', function () {
-        $(this).toggleClass("is-active");
-    });
+    //$(".hamburger").on('click', function () {
+    //    $(this).toggleClass("is-active");
+    //});
 
 
     /*
     -------------------
     List item active
     -------------------*/
-    $('.header li, .sidebar li').on('click', function () {
-        $(".header li.active, .sidebar li.active").removeClass("active");
-        $(this).addClass('active');
-    });
+    //$('.header li, .sidebar li').on('click', function () {
+    //    $(".header li.active, .sidebar li.active").removeClass("active");
+    //    $(this).addClass('active');
+    //});
 
-    $(".header li").on("click", function (event) {
-        event.stopPropagation();
-    });
+    //$(".header li").on("click", function (event) {
+    //    event.stopPropagation();
+    //});
 
-    $(document).on("click", function () {
-        $(".header li").removeClass("active");
+    //$(document).on("click", function () {
+    //    $(".header li").removeClass("active");
 
-    });
+    //});
 
 
 
@@ -100,17 +100,17 @@
     /*  Chat Sidebar User custom Search
     ---------------------------------------*/
 
-    $('[data-search]').on('keyup', function () {
-        var searchVal = $(this).val();
-        var filterItems = $('[data-filter-item]');
+    //$('[data-search]').on('keyup', function () {
+    //    var searchVal = $(this).val();
+    //    var filterItems = $('[data-filter-item]');
 
-        if (searchVal != '') {
-            filterItems.addClass('hidden');
-            $('[data-filter-item][data-filter-name*="' + searchVal.toLowerCase() + '"]').removeClass('hidden');
-        } else {
-            filterItems.removeClass('hidden');
-        }
-    });
+    //    if (searchVal != '') {
+    //        filterItems.addClass('hidden');
+    //        $('[data-filter-item][data-filter-name*="' + searchVal.toLowerCase() + '"]').removeClass('hidden');
+    //    } else {
+    //        filterItems.removeClass('hidden');
+    //    }
+    //});
 
 
         /*  Chackbox all
