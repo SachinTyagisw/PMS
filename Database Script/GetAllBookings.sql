a4eca4cc9903e86f802321936068526b2d54cde4
DESKTOP-93PK18V\sachi <sachin.tyagisw@gmail.com>
2017-06-29 11:46:47 -0700

    GetAllBookings Fix

--- Database Script/GetAllBookings.sql
+++ Database Script/GetAllBookings.sql
@@ -1,46 +1,45 @@
--- =============================================      
--- Author:  Sachin Tyagi      
--- Create date: April 29, 2017      
--- Description: This stored procedure shall get the details of booking based on the checkin and checkoutdate      
--- =============================================      
---Exec GETALLBOOKINGS 1, '2017-06-03', '2017-06-23'
-
-ALTER PROCEDURE [dbo].[GETALLBOOKINGS]      
- @PROPERTYID INT,      
- @CHECKINTIME DATETIME = NULL,      
- @CHECKOUTDATE DATETIME = NULL      
-AS      
-BEGIN      
-    
-IF(CONVERT(TIME,@CHECKOUTDATE)= '00:00:00.0000000')    
- set @CHECKOUTDATE = @CHECKOUTDATE + '23:59:59'    
-    
-SELECT       
-  distinct BK.ID      
- ,BK.PROPERTYID      
- ,BK.CHECKINTIME      
- ,BK.CHECKOUTTIME      
- ,GUEST.FIRSTNAME      
- ,GUEST.LASTNAME    
- ,GUEST.ID AS GUESTID    
- ,RB.ID AS ROOMBOOKINGID    
- ,ROOM.ID AS ROOMID    
- ,ROOM.Number AS ROOMNUMBER    
-     
- FROM BOOKING BK INNER JOIN ROOMBOOKING RB       
- ON BK.ID = RB.BOOKINGID      
- INNER JOIN Room    
- ON RB.RoomID = Room.ID    
- INNER JOIN GUEST      
- ON RB.GUESTID = GUEST.ID      
- WHERE (BK.CHECKINTIME >= ISNULL(@CHECKINTIME,'1900-01-01')      
- AND BK.CHECKINTIME <= ISNULL(stuff(convert(varchar(19), @CHECKOUTDATE, 126),11,1,' '), GETDATE())) 
- OR
- ((BK.CHECKINTIME <= ISNULL(@CHECKINTIME,'1900-01-01') and BK.CheckoutTime >= ISNULL(stuff(convert(varchar(19), @CHECKOUTDATE, 126),11,1,' '), GETDATE())))
- OR
- ((BK.CHECKINTIME <= ISNULL(@CHECKINTIME,'1900-01-01') and BK.CheckoutTime <= ISNULL(stuff(convert(varchar(19), @CHECKOUTDATE, 126),11,1,' '), GETDATE())))
- OR
- ((BK.CHECKINTIME <= ISNULL(@CHECKOUTDATE,'1900-01-01') and BK.CheckoutTime >= ISNULL(stuff(convert(varchar(19), @CHECKOUTDATE, 126),11,1,' '), GETDATE())))
+-- =============================================        
+-- Author:  Sachin Tyagi        
+-- Create date: April 29, 2017        
+-- Description: This stored procedure shall get the details of booking based on the checkin and checkoutdate        
+-- =============================================        
+--Exec GETALLBOOKINGS 1, '2017-05-26', '2017-06-13'
  
- AND BK.ISACTIVE=1      
+ALTER PROCEDURE [dbo].[GETALLBOOKINGS]        
+ @PROPERTYID INT,        
+ @CHECKINTIME DATETIME = NULL,        
+ @CHECKOUTDATE DATETIME = NULL        
+AS        
+BEGIN        
+      
+IF(CONVERT(TIME,@CHECKOUTDATE)= '00:00:00.0000000')      
+ set @CHECKOUTDATE = @CHECKOUTDATE + '23:59:59'      
+      
+SELECT         
+  distinct BK.ID        
+ ,BK.PROPERTYID        
+ ,BK.CHECKINTIME        
+ ,BK.CHECKOUTTIME        
+ ,GUEST.FIRSTNAME        
+ ,GUEST.LASTNAME      
+ ,GUEST.ID AS GUESTID      
+ ,RB.ID AS ROOMBOOKINGID      
+ ,ROOM.ID AS ROOMID      
+ ,ROOM.Number AS ROOMNUMBER      
+       
+ FROM BOOKING BK INNER JOIN ROOMBOOKING RB         
+ ON BK.ID = RB.BOOKINGID        
+ INNER JOIN Room      
+ ON RB.RoomID = Room.ID      
+ INNER JOIN GUEST        
+ ON RB.GUESTID = GUEST.ID        
+ WHERE (BK.CHECKINTIME >= ISNULL(@CHECKINTIME,'1900-01-01') AND BK.CHECKOUTTIME <= ISNULL(stuff(convert(varchar(19), @CHECKOUTDATE, 126),11,1,' '), GETDATE())) 
+ OR  
+  ((BK.CHECKINTIME <= ISNULL(@CHECKINTIME,'1900-01-01') and BK.CheckoutTime >= ISNULL(stuff(convert(varchar(19), @CHECKOUTDATE, 126),11,1,' '), GETDATE())))  
+ OR  
+ ((BK.CHECKINTIME <= ISNULL(@CHECKINTIME,'1900-01-01') and ((BK.CheckoutTime <= ISNULL(stuff(convert(varchar(19), @CHECKOUTDATE, 126),11,1,' '), GETDATE())) and BK.CheckoutTime >= ISNULL(@CHECKINTIME,'1900-01-01'))))
+ OR  
+ ((BK.CHECKINTIME <= ISNULL(@CHECKOUTDATE,'1900-01-01') and BK.CheckoutTime >= ISNULL(stuff(convert(varchar(19), @CHECKOUTDATE, 126),11,1,' '), GETDATE())))  
+   
+ AND BK.ISACTIVE=1        
 END 
