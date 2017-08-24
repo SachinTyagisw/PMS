6627675707f0142005efb22599b5956e84d0fcd9
vipul83 <vipulkumarsharma06@gmail.com>
2017-08-24 23:23:53 +0530

    adding missing file

---
+++ PMS.Resources.DTO/Request/UpdateStatusRequestDto.cs
@@ -0,0 +1,20 @@
+﻿using PMS.Resources.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMS.Resources.DTO.Request
+{
+    public class UpdateStatusRequestDto
+    {
+        [DataMember]
+        public int BookingId { get; set; }
+        [DataMember]
+        public bool Status { get; set; }
+        [DataMember]
+        public int PropertyId { get; set; }
+    }
+}
