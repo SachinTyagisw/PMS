08df692403dba47f5ac7dea1499d653307da7bc0
DESKTOP-93PK18V\sachi <sachin.tyagisw@gmail.com>
2017-10-31 10:13:36 -0700

    Shift Report Stored procedures

--- Database Script/GetConsolidatedShiftReport.sql
+++ Database Script/GetConsolidatedShiftReport.sql
@@ -1,33 +1,219 @@
-Create PROC [dbo].[GetConsolidatedShiftReport]             
-@StartDate Datetime = NULL,  
-@EndDate DateTime = NULL,  
-@PropertyId nvarchar(max) = NULL      
-AS BEGIN  
 
-If(OBJECT_ID('tempdb..#tmpData') Is Not Null)
-Begin
-    Drop Table #tmpData
-End
+ALTER PROC [dbo].[GetConsolidatedShiftReport]               
+@StartDate Datetime,    
+@EndDate DateTime,    
+@PropertyId nvarchar(max)        
+AS BEGIN    
+  
+  
+If(OBJECT_ID('tempdb..#tmpData') Is Not Null)  
+Begin  
+    Drop Table #tmpData  
+End  
+  
+If(OBJECT_ID('tempdb..#tmp_Table') Is Not Null)  
+Begin  
+    Drop Table #tmp_Table  
+End  
+  
+If(OBJECT_ID('tempdb..#Taxdata') Is Not Null)  
+Begin  
+    Drop Table #Taxdata  
+End  
+  
+If(OBJECT_ID('tempdb..#PaymentData') Is Not Null)  
+Begin  
+    Drop Table #PaymentData  
+End  
+  
+  
+create table #tmpData  
+(  
+ DataType nvarchar(max),  
+ ADR money,  
+ RevPAR money,  
+ RoomCharges money,  
+ ConsolidatedTax money  
+ )  
+   
+   
+create table #tmp_Table  
+(  
+ CheckinTime Datetime,  
+ CheckoutTime Datetime,  
+ TransactionNumber int,  
+ RoomNumber nvarchar(200),  
+ RoomCharges money,  
+ GuestName nvarchar(max),  
+ GuestRemarks nvarchar(max),  
+ TotalAmount money  
+ )  
+   
+IF OBJECT_ID('tempdb.dbo.#Taxdata', 'U') IS NOT NULL      
+drop table #Taxdata   
+   
+select distinct TaxShortName, IDENTITY(int,1,1) as ID into #Taxdata from InvoiceTaxDetail  where COALESCE(PATINDEX('%[^ ]%', [TaxShortName]), 0) > 0    
+   
+Declare @Script as NVarchar(max);      
+Declare @Script_prepare as NVarchar(max);      
+      
+Set @Script_prepare = 'Alter table #tmp_Table Add [?] money;'      
+Set @Script = ''      
+       
+Select @Script = @Script + Replace(@Script_prepare, '?', Replace([TaxShortName], ' ',''))      
+From #Taxdata      
+  
+Exec (@Script)   
+  
+select distinct PaymentMode, IDENTITY(int,1,1) as ID into #PaymentData from invoicepaymentdetails where COALESCE(PATINDEX('%[^ ]%', [PaymentMode]), 0) > 0    
+      
+Set @Script_prepare = 'Alter table #tmp_Table Add [?] money;'      
+Set @Script = ''      
+       
+Select @Script = @Script + Replace(@Script_prepare, '?', [dbo].RemoveAllSpaces([PaymentMode]))      
+From #PaymentData      
+      
+Exec (@Script)         
+  
+ --Fetch data from Stored-Procedure for Today  
+ Insert into #tmp_Table  
+ Exec GetShiftReport @StartDate,@EndDate,@PropertyId  
+   
+ Declare @RoomCharges money  
+ Declare @TotalRentedRooms int  
+ Declare @TotalRentableRooms int  
+   
+ set @RoomCharges = (select SUM(ISNULL(RoomCharges,0)) from #tmp_Table)  
+ set @TotalRentedRooms = (select COUNT(1) from #tmp_Table)   
+ set @TotalRentableRooms = (select COUNT(1) from Room)  
+   
+ Declare @ADR money  
+ Declare @RevPAR money  
+ Declare @consolidatedTax money  
+   
+ set @ADR = @RoomCharges /@TotalRentedRooms  
+ set @RevPAR = @RoomCharges / @TotalRentableRooms  
+   
+ Declare @LoopCounter int, @MaxTaxId int,@MinTaxId int, @TaxName nvarchar(200)  
+ Select @LoopCounter = MIN(ID),@MinTaxId = MIN(ID), @MaxTaxId = MAX(ID) from #Taxdata  
+   
+ set @Script = 'Select @consolidatedTax1= '  
+   
+ while(@LoopCounter is not null and @LoopCounter <=@MaxTaxId)  
+ Begin  
+  select @TaxName = TaxShortName from #Taxdata where ID = @LoopCounter  
+  if(COALESCE(PATINDEX('%[^ ]%', @TaxName), 0) > 0)  
+  Begin  
+  if(@MinTaxId = @LoopCounter)  
+  Set @Script = @Script + ' SUM(' + @TaxName +')'  
+  else  
+   Set @Script = @Script + '+ SUM(' + @TaxName +')'  
+  End  
+  set @LoopCounter = @LoopCounter +1  
+ End  
+   
+ set @Script = @Script + ' from #tmp_Table'  
+ --Print(@Script)   
+   
+ exec sp_executesql @Script, N'@consolidatedTax1 money out', @consolidatedTax out  
+ --print @consolidatedTax  
+   
+ insert into #tmpData values('Today',@ADR,@RevPAR,@RoomCharges,@consolidatedTax)  
+   
+ --Fetch data from Stored-Procedure for Month  
+   
+ If(OBJECT_ID('tempdb..#tmp_Table') Is Not Null)  
+Begin  
+    Truncate Table #tmp_Table  
+End  
+  
+SELECT @StartDate = DATEADD(month, DATEDIFF(month, 0, @EndDate), 0)  
+  
+Insert into #tmp_Table  
+ Exec GetShiftReport @StartDate,@EndDate,@PropertyId  
+   
+set @RoomCharges = (select SUM(ISNULL(RoomCharges,0)) from #tmp_Table)  
+set @TotalRentedRooms = (select COUNT(1) from #tmp_Table)   
+set @TotalRentableRooms = (select COUNT(1) from Room)  
+   
+ set @ADR = @RoomCharges /@TotalRentedRooms  
+ set @RevPAR = @RoomCharges / @TotalRentableRooms  
+   
+ Select @LoopCounter = MIN(ID),@MinTaxId = MIN(ID), @MaxTaxId = MAX(ID) from #Taxdata  
+  
+ set @Script = 'Select @consolidatedTax1= '  
+   
+ while(@LoopCounter is not null and @LoopCounter <=@MaxTaxId)  
+ Begin  
+  select @TaxName = TaxShortName from #Taxdata where ID = @LoopCounter  
+  if(COALESCE(PATINDEX('%[^ ]%', @TaxName), 0) > 0)  
+  Begin  
+  if(@MinTaxId = @LoopCounter)  
+  Set @Script = @Script + ' SUM(' + @TaxName +')'  
+  else  
+   Set @Script = @Script + '+ SUM(' + @TaxName +')'  
+  End  
+  set @LoopCounter = @LoopCounter +1  
+ End  
+   
+ set @Script = @Script + ' from #tmp_Table'  
+ --Print(@Script)   
+   
+ exec sp_executesql @Script, N'@consolidatedTax1 money out', @consolidatedTax out  
+ --print @consolidatedTax  
+   
+ insert into #tmpData values('Monthly',@ADR,@RevPAR,@RoomCharges,@consolidatedTax)  
+   
+--Fetch data from Stored-Procedure for Yearly  
+   
+ If(OBJECT_ID('tempdb..#tmp_Table') Is Not Null)  
+Begin  
+    Truncate Table #tmp_Table  
+End  
+  
+SELECT @StartDate = DATEADD(yy, DATEDIFF(yy, 0, @EndDate), 0)  
+  
+Insert into #tmp_Table  
+ Exec GetShiftReport @StartDate,@EndDate,@PropertyId  
+   
+set @RoomCharges = (select SUM(ISNULL(RoomCharges,0)) from #tmp_Table)  
+set @TotalRentedRooms = (select COUNT(1) from #tmp_Table)   
+set @TotalRentableRooms = (select COUNT(1) from Room)  
+   
+ set @ADR = @RoomCharges /@TotalRentedRooms  
+ set @RevPAR = @RoomCharges / @TotalRentableRooms  
+   
+ Select @LoopCounter = MIN(ID),@MinTaxId = MIN(ID), @MaxTaxId = MAX(ID) from #Taxdata  
 
-create table #tmpData
-(
- DataType nvarchar(max),
- ADR money,
- RevPAR money,
- RoomCharges money,
- ConsolidatedTax money
- )
- 
- --Dummy Data
- insert into #tmpData values('Today',44.44,13.33,400.00,56.5)
- insert into #tmpData values('Monthly',144.44,113.33,800.00,90.5)
- insert into #tmpData values('Yearly',1444.44,1013.33,1200.00,9889.5)
- 
- select
- DataType,
- ADR,
- RevPAR,
- RoomCharges,
- ConsolidatedTax from #tmpData
-    
-END    
+ set @Script = 'Select @consolidatedTax1= '  
+   
+ while(@LoopCounter is not null and @LoopCounter <=@MaxTaxId)  
+ Begin  
+  select @TaxName = TaxShortName from #Taxdata where ID = @LoopCounter  
+  if(COALESCE(PATINDEX('%[^ ]%', @TaxName), 0) > 0)  
+  Begin  
+  if(@MinTaxId = @LoopCounter)  
+  Set @Script = @Script + ' SUM(' + @TaxName +')'  
+  else  
+   Set @Script = @Script + '+ SUM(' + @TaxName +')'  
+  End  
+  set @LoopCounter = @LoopCounter +1  
+ End  
+   
+ set @Script = @Script + ' from #tmp_Table'  
+ --Print(@Script)   
+   
+ exec sp_executesql @Script, N'@consolidatedTax1 money out', @consolidatedTax out  
+ --print @consolidatedTax  
+   
+ insert into #tmpData values('Yearly',@ADR,@RevPAR,@RoomCharges,@consolidatedTax)  
+  
+   
+ select  
+ DataType,  
+ ADR,  
+ RevPAR,  
+ RoomCharges,  
+ ConsolidatedTax from #tmpData  
+END      
+  
--- Database Script/GetShiftReport.sql
+++ Database Script/GetShiftReport.sql
@@ -1,61 +1,65 @@
-Create PROC [GetShiftReport]           
-@StartDate Datetime = NULL,
-@EndDate DateTime = NULL,
-@PropertyId nvarchar(max) = NULL    
-AS BEGIN
 
-declare @sql varchar(8000),@col_list varchar(8000),@pivot_listTax varchar(8000),@pivot_listPayment varchar (8000), @pivot_listRoomCharge varchar(8000)
-set @pivot_listTax =stuff((select distinct  ','+quotename(TaxShortName) from InvoiceTaxDetail where TaxShortName <> ''  for xml path('')),1,1,'')
-print @pivot_listTax
-set @pivot_listPayment = stuff((select distinct  ','+quotename(PaymentMode) from InvoicePaymentDetails where PaymentMode <> '' for xml path('')),1,1,'')
-print @pivot_listPayment
+ALTER PROC [dbo].[GetShiftReport]               
+@StartDate Datetime,    
+@EndDate DateTime,    
+@PropertyId nvarchar       
+AS BEGIN  
+  
+set @StartDate = cast(@StartDate as DATE)  
+set @EndDate= cast(convert(char(8), @EndDate, 112) + ' 23:59:59.99' as datetime)  
 
-set @sql  = 'select * from
-(
-SELECT 
-bk.CheckinTime,
-bk.CheckoutTime,bk.id as TransantionNumber,
-Room.Number as RoomNumber,
-[dbo].fnGetRoomCharge(inv.ID) as RoomCharge,
-Guest.FirstName +'' ''+ Guest.LastName as GuestName, 
-bk.GuestRemarks,invTax.TaxShortName,invTax.TaxAmount, invPayment.PaymentMode, invPayment.PaymentValue,inv.TotalAmount
-FROM Booking bk   
-inner join   
-Invoice inv on  
-inv.BookingID = bk.ID  
-inner join  
-InvoicePaymentDetails invPayment on  
-invPayment.invoiceid = inv.ID  
-inner join   
-InvoiceItems on  
-InvoiceItems.InvoiceID = inv.ID  
-inner join  
-InvoiceTaxDetail invTax on  
-invTax.InvoiceID = inv.ID  
-inner join  
-RoomBooking rbk on  
-rbk.BookingID = bk.ID  
-inner join  
-Room on  
-rbk.RoomID = Room.ID  
-inner join  
-RoomType rt on  
-rt.ID = Room.RoomTypeID  
-LEFT OUTER JOIN               
-Guest               
-ON rbk.GuestID = Guest.ID            
-) as p
-pivot 
-(
-max(TaxAmount)
-for TaxShortName in ( '+@pivot_listTax+') 
-)as pvt 
-pivot
-(
-max(PaymentValue)
-for PaymentMode in ( '+@pivot_listPayment+')
+declare @sql varchar(8000),@col_list varchar(8000),@pivot_listTax varchar(8000),@pivot_listPayment varchar (8000), @pivot_listRoomCharge varchar(8000)    
+set @pivot_listTax =stuff((select distinct  ','+quotename(TaxShortName) from InvoiceTaxDetail where TaxShortName <> ''  for xml path('')),1,1,'')    
+
+set @pivot_listPayment = stuff((select distinct  ','+quotename(PaymentMode) from InvoicePaymentDetails where PaymentMode <> '' for xml path('')),1,1,'')    
+
+set @sql  = 'select * from    
+(    
+SELECT     
+bk.CheckinTime,    
+bk.CheckoutTime,bk.id as TransantionNumber,    
+Room.Number as RoomNumber,    
+[dbo].fnGetRoomCharge(inv.ID) as RoomCharge,    
+Guest.FirstName +'' ''+ Guest.LastName as GuestName,     
+bk.GuestRemarks,invTax.TaxShortName,invTax.TaxAmount, invPayment.PaymentMode, invPayment.PaymentValue,inv.TotalAmount    
+FROM Booking bk
+inner join       
+Invoice inv on      
+inv.BookingID = bk.ID      
+inner join      
+InvoicePaymentDetails invPayment on      
+invPayment.invoiceid = inv.ID      
+inner join       
+InvoiceItems on      
+InvoiceItems.InvoiceID = inv.ID      
+inner join      
+InvoiceTaxDetail invTax on      
+invTax.InvoiceID = inv.ID      
+inner join      
+RoomBooking rbk on      
+rbk.BookingID = bk.ID      
+inner join      
+Room on      
+rbk.RoomID = Room.ID      
+inner join      
+RoomType rt on      
+rt.ID = Room.RoomTypeID      
+LEFT OUTER JOIN                   
+Guest                   
+ON rbk.GuestID = Guest.ID where bk.propertyID =' + @PropertyId + 
+' and bk.checkinTime > ''' + CONVERT(VARCHAR(max), @StartDate, 120) + ''' and bk.checkinTime < ''' + CONVERT(VARCHAR(max), @EndDate, 120) + '''
+) as p    
+pivot     
+(    
+max(TaxAmount)    
+for TaxShortName in ( '+@pivot_listTax+')     
+)as pvt     
+pivot    
+(    
+max(PaymentValue)    
+for PaymentMode in ( '+@pivot_listPayment+')    
 ) as pvt1'
-
---print @sql
-exec(@sql)
-END
+    
+--print @sql    
+exec(@sql)    
+END  
