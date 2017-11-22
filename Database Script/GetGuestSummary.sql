4dcfe5c217a5be30394ea7ce279ecd242fda2cda
DESKTOP-93PK18V\sachi <sachin.tyagisw@gmail.com>
2017-11-22 08:51:28 -0800

    Minor fix

--- Database Script/GetGuestSummary.sql
+++ Database Script/GetGuestSummary.sql
@@ -1,39 +1,40 @@
-ALTER PROCEDURE [dbo].[GETGuestSummary]                      
- @PROPERTYID INT,                      
- @CURRENTDATE DATETIME  
-AS                      
-BEGIN                      
-             
-SELECT                       
-  distinct BK.ID                      
- ,BK.PROPERTYID                      
- ,BK.CHECKINTIME                      
- ,BK.CHECKOUTTIME            
- ,BK.Status as 'Status'
- ,[dbo].fnGetPaymentOption(Invoice.ID) as 'ModeOFPayment'  
- ,[dbo].fnGetRoomRates(Invoice.ID) as 'Rate'  
- ,GUEST.FIRSTNAME                      
- ,GUEST.LASTNAME                    
- ,GUEST.ID AS GUESTID                    
- ,RB.ID AS ROOMBOOKINGID                    
- ,ROOM.ID AS ROOMID                    
- ,ROOM.Number AS ROOMNUMBER         
- ,ROOM.RoomTypeID as RoomTypeID        
- ,RoomType.NAME as RoomTypeName                     
- ,RoomType.ShortName as RoomTypeShortName    
- ,ROOM.Status as RoomStatus  
- ,Invoice.TotalAmount as TotalAmount 
- ,getdate() as NextReservation
- FROM BOOKING BK INNER JOIN ROOMBOOKING RB                       
- ON BK.ID = RB.BOOKINGID   
- INNER JOIN Invoice  
- ON BK.ID = Invoice.BOOKINGID  
- INNER JOIN Room                    
- ON RB.RoomID = Room.ID        
- Inner JOIN RoomType        
- On  Room.RoomTypeID = RoomType.ID                   
- INNER JOIN GUEST                      
- ON RB.GUESTID = GUEST.ID                      
- WHERE (BK.CHECKINTIME >= @CURRENTDATE AND BK.CHECKOUTTIME <=  BK.CheckoutTime )                    
-            
-END   
+    
+ALTER PROCEDURE [dbo].[GETGuestSummary]                        
+ @PROPERTYID INT,                        
+ @CURRENTDATE DATETIME    
+AS                        
+BEGIN                        
+               
+SELECT                         
+  distinct BK.ID                        
+ ,BK.PROPERTYID                        
+ ,BK.CHECKINTIME                        
+ ,BK.CHECKOUTTIME              
+ ,BK.Status as 'Status'  
+ ,[dbo].fnGetPaymentOption(Invoice.ID) as 'ModeOFPayment'    
+ ,[dbo].fnGetRoomRates(Invoice.ID) as 'Rate'    
+ ,GUEST.FIRSTNAME                        
+ ,GUEST.LASTNAME                      
+ ,GUEST.ID AS GUESTID                      
+ ,RB.ID AS ROOMBOOKINGID                      
+ ,ROOM.ID AS ROOMID                      
+ ,ROOM.Number AS ROOMNUMBER           
+ ,ROOM.RoomTypeID as RoomTypeID          
+ ,RoomType.NAME as RoomTypeName                       
+ ,RoomType.ShortName as RoomTypeShortName      
+ ,ROOM.Status as RoomStatus    
+ ,Invoice.TotalAmount as TotalAmount   
+ ,getdate() as NextReservation  
+ FROM BOOKING BK INNER JOIN ROOMBOOKING RB                         
+ ON BK.ID = RB.BOOKINGID     
+ INNER JOIN Invoice    
+ ON BK.ID = Invoice.BOOKINGID    
+ INNER JOIN Room                      
+ ON RB.RoomID = Room.ID          
+ Inner JOIN RoomType          
+ On  Room.RoomTypeID = RoomType.ID                     
+ INNER JOIN GUEST                        
+ ON RB.GUESTID = GUEST.ID                        
+ WHERE (BK.CHECKINTIME <= @CURRENTDATE AND BK.CHECKOUTTIME >=  @CURRENTDATE )                      
+              
+END 
